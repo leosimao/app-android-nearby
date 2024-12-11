a6f618fa5ad76fc29b7a791469a80f36b5ff9cc7
Leonardo Simao <leonardosc.me@gmail.com>
2024-12-11 00:03:28 -0300

    Iniciando Primeira Aplicação Android

---
+++ app/src/main/java/com/deveasy/nearby/data/model/Rule.kt
@@ -0,0 +1,7 @@
+package com.deveasy.nearby.data.model
+
+data class Rule(
+    val id: String,
+    val description: String,
+    val marketId: String
+)
---
+++ app/src/main/java/com/deveasy/nearby/data/model/mock/MockCategory.kt
@@ -0,0 +1,26 @@
+package com.deveasy.nearby.data.model.mock
+
+import com.deveasy.nearby.data.model.Category
+
+val mockCategory = listOf(
+    Category (
+        id = "1",
+        name = "Alimentação"
+    ),
+    Category (
+        id = "2",
+        name = "Cinema"
+    ),
+    Category (
+        id = "4",
+        name = "Padaria"
+    ),
+    Category (
+        id = "6",
+        name = "Combustível"
+    ),
+    Category (
+        id = "7",
+        name = "Compras"
+    )
+)
